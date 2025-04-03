e4368fc66f8dd4758cccfc5a807a94dbb8ed61cc
PrzemKaleta <przemyslaw.kaleta@student.wsb.edu.pl>
2025-04-03 18:46:22 +0200

    zad_1

--- PPZO_63514_kaleta/PPZO_63514_kaleta.py
+++ PPZO_63514_kaleta/PPZO_63514_kaleta.py
@@ -1,1 +1,39 @@
+﻿
+print("Menu")
 
+print("1 - Prosty kalkulator dwoch liczb")
+print("2 - Konwerter temperatur (Celsjusz ↔ Fahrenheit)")
+print("3 - Srednia ocen ucznia")
+
+menu_option = int(input("Opcja: "))
+
+def switch_menu(menu_option):
+    match menu_option:
+        case 1:
+             print("Prosty kalkulator dwoch liczb")
+             a = int(input("Podaj pierwsza liczbe: "))
+             b = int(input("Podaj druga liczbe: "))
+             print("Podaj jaka operacje arytmetyczna chcesz wykonac (+, -, *, /): ")
+             zad1_option = input()
+             if zad1_option == "+":
+                wynik1 = a + b
+             elif zad1_option == "-":
+                wynik1 = a - b
+             elif zad1_option == "*":
+                wynik1 = a * b
+             elif zad1_option == "/":
+                 if b != 0:
+                     wynik1 = a / b
+                 else:
+                     return ("Nie mozna dzielic przez 0")
+             else:
+                 return "Bledna operacja arytmetyczna" 
+             return (f"Wynik = {wynik1}")
+        case 2:
+             return "Menu2"
+        case 3:
+             return "Menu3"
+        case _:
+             return "Niepoprawna opcja"
+
+print(switch_menu(menu_option))
